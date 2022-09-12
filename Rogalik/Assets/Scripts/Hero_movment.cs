83d15dab2cc26aa7c7466210ea274ca5f2541b80
ваше имя <email@example.com>
2022-09-13 00:33:51 +0700

    aim_movment2
    Поменял скрипт передвижения и закрепил анимации покоя

--- Rogalik/Assets/Scripts/Hero_movment.cs
+++ Rogalik/Assets/Scripts/Hero_movment.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class Hero_movment : MonoBehaviour
 {
-    public float  speed = 1f;
-    public float jampForse = 5f;
+    public Vector2 moveVector;
+    public float speed = 2f;
 
     Rigidbody2D rb;
-    SpriteRenderer sr;
 
     void Start()
     {
         rb = GetComponent <Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movment = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movment, 0, 0) * speed * Time.deltaTime;
-
-        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.05f)
-            rb.AddForce(new Vector2(0, jampForse), ForceMode2D.Impulse);
-
-        sr.flipX = movment < 0 ? true : false;
+        walk();
     }
+
+    void walk()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
+    }
+
 }
